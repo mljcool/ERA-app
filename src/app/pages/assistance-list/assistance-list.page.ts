b60243564c686a7a7564f1169587eb78c3daea85
MarkGocela <markloujames.gocela@devpartners.co>
2020-02-18 23:59:40 +0800

    add destroyer for subscriber

--- src/app/pages/assistance-list/assistance-list.page.ts
+++ src/app/pages/assistance-list/assistance-list.page.ts
@@ -1,33 +1,46 @@
+import { takeUntil } from 'rxjs/operators';
 import { Router, NavigationExtras } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { IAssistance } from 'src/app/models/assistance.model';
 import { AssistanceService } from 'src/app/modals/assistance/assistance.service';
 import { StoragUserDataService } from 'src/app/services/storages/storage-user-services';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-assistance-list',
   templateUrl: './assistance-list.page.html',
   styleUrls: ['./assistance-list.page.scss']
 })
-export class AssistanceListPage implements OnInit {
+export class AssistanceListPage implements OnInit, OnDestroy {
   public searchTerm = '';
   assistanceLists: IAssistance[];
   shops: IAssistance[] = [];
   copyShops: IAssistance[] = [];
   isLoading = true;
 
+  private unsubscribeAll: Subject<any>;
+
   constructor(
     private assistanceService: AssistanceService,
     private googleStorageUser: StoragUserDataService,
     private router: Router
-  ) { }
+  ) {
+
+    this.unsubscribeAll = new Subject();
+
+  }
 
   ngOnInit() { }
 
+  ngOnDestroy(): void {
+    // Unsubscribe from all subscriptions
+    this.unsubscribeAll.next();
+    this.unsubscribeAll.complete();
+  }
   ionViewWillEnter() {
     this.googleStorageUser.getObjectGoogleUsers().then(data => {
       this.assistanceService
-        .getAllAssistance(data.id)
+        .getAllAssistance(data.id).pipe(takeUntil(this.unsubscribeAll))
         .subscribe(assistanceLists => {
           this.assistanceLists = assistanceLists;
           this.shops = this.copyShops = assistanceLists;
