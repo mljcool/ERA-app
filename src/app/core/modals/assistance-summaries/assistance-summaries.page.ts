15bba6767ec06659663f777eb3d761b96b63e08c
MarkGocela <markloujames.gocela@devpartners.co>
2020-07-23 00:15:51 +0800

    add confirmationed

--- src/app/core/modals/assistance-summaries/assistance-summaries.page.ts
+++ src/app/core/modals/assistance-summaries/assistance-summaries.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { NavParams } from '@ionic/angular';
+import { NavParams, AlertController, ModalController } from '@ionic/angular';
 import { assistTanceList } from 'src/app/constants/assistanceTypes';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-assistance-summaries',
@@ -9,18 +9,55 @@ import { assistTanceList } from 'src/app/constants/assistanceTypes';
   styleUrls: ['./assistance-summaries.page.scss'],
 })
 export class AssistanceSummariesPage implements OnInit {
-
+  isConfirming = false;
+  afterConfirmed = false;
   serviceDetails = {};
   shopDetail = {};
-  constructor(private navParams: NavParams) {
-    const { shopDetail, serviceTypeParam } = this.navParams.get('assistanceDetails')
-    this.serviceDetails = assistTanceList.find(detail => detail.id === parseInt(serviceTypeParam, 10));
+  constructor(
+    private navParams: NavParams,
+    public alertController: AlertController,
+    public modaCtrl: ModalController,
+    private router: Router
+  ) {
+    const { shopDetail, serviceTypeParam } = this.navParams.get(
+      'assistanceDetails'
+    );
+    this.serviceDetails = assistTanceList.find(
+      (detail) => detail.id === parseInt(serviceTypeParam, 10)
+    );
     this.shopDetail = shopDetail;
-    console.log('serviceDetails', this.serviceDetails);
-    console.log('shopDetail', shopDetail);
   }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  onConfirm() {
+    this.isConfirming = true;
+    const confirming = setTimeout(() => {
+      this.isConfirming = false;
+      clearTimeout(confirming);
+      this.afterConfirmed = true;
+      this.presentAlert();
+    }, 1000);
   }
 
+  okayClose() {
+    this.modaCtrl.dismiss();
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Info',
+      subHeader: 'Assistance',
+      message: 'Your request successfully sent.',
+      buttons: [{
+        text: 'Okay',
+        handler: () => {
+          this.router.navigate(['/main-menu'])
+        }
+      }],
+    });
+
+    await alert.present();
+  }
 }
