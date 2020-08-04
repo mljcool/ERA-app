1a8c9324f4f408864ebde0c7fb25b83b016f1e37
MarkGocela <markloujames.gocela@devpartners.co>
2020-08-05 02:01:36 +0800

    renames labels and adding new uis

--- src/app/core/pages/shop-details/shop-details.page.ts
+++ src/app/core/pages/shop-details/shop-details.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, NavigationExtras } from '@angular/router';
 
 @Component({
   selector: 'app-shop-details',
@@ -7,13 +7,29 @@ import { Router } from '@angular/router';
   styleUrls: ['./shop-details.page.scss'],
 })
 export class ShopDetailsPage implements OnInit {
-
   constructor(private router: Router) { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
   onBack(): void {
     this.router.navigateByUrl('/main-menu');
+  }
+
+  onOrderItems(): void {
+    const navigationExtras: NavigationExtras = {
+      queryParams: {
+        shopId: 1,
+      },
+    };
+    this.router.navigate(['/make-orders'], navigationExtras);
+  }
+
+  onAppoinment(): void {
+    const navigationExtras: NavigationExtras = {
+      queryParams: {
+        shopId: 1,
+      },
+    };
+    this.router.navigate(['/make-appointment'], navigationExtras);
   }
 }
